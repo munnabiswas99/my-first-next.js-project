9d39c2597db77f36360b54ed7d0b6bd8f024c549
munnabiswas99 <biswas15-5261@diu.edu.bd>
2026-05-20 23:46:58 +0600

    Image component explored

--- app/about/mission/page.js
+++ app/about/mission/page.js
@@ -1,11 +1,14 @@
 import Button from '@/app/components/Button';
+import Image from 'next/image';
 import React from 'react';
+import img1 from '@/public/images/img1.png';
 
 const page = () => {
     // throw new Error("There was an error");
     return (
         <div>
             <h1 className='my-2'>This is mission page</h1>
+            <Image src={img1} alt='dashboard'></Image>
             <Button></Button>
         </div>
     );
