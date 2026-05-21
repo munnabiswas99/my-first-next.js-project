50cdce32544cee05790f55b6285a39fdc79e95a1
munnabiswas99 <biswas15-5261@diu.edu.bd>
2026-05-21 18:13:09 +0600

    Metadata Exploring

--- app/about/layout.js
+++ app/about/layout.js
@@ -1,6 +1,11 @@
 import Link from "next/link";
 import React from "react";
 
+export const metadata = {
+  title: "About",
+  description: "This is about page  ",
+};
+
 const layout = ({ children }) => {
   return (
     <div>
